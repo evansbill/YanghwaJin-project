d1b9e0bf569e82c1ff7878bbfc13b83a0d04a0e8
evansbill <21500739@handong.edu>
2021-02-15 22:33:32 +0900

    Initial Commit

---
+++ YanghwaJin/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  YanghwaJin
+//
+//  Created by 최용규 on 2021/02/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ YanghwaJin/YanghwaJinApp.swift
@@ -0,0 +1,17 @@
+//
+//  YanghwaJinApp.swift
+//  YanghwaJin
+//
+//  Created by 최용규 on 2021/02/15.
+//
+
+import SwiftUI
+
+@main
+struct YanghwaJinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
